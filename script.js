242dfc731be833c770360abb183df5538c070321
Anisha <ani25sha@gmail.com>
2026-08-09 22:58:33 +0530

    board modification

--- script.js
+++ script.js
@@ -5,8 +5,16 @@ const blockWidth = 30
 const cols=Math.floor(board.clientWidth/blockWidth);
 const rows=Math.floor(board.clientHeight/blockHeight);
 
-for(let i=0; i<rows*cols; i++){
+/*for(let i=0; i<rows*cols; i++){
     const block = document.createElement('div');
     block.classList.add("block")
     board.appendChild(block);
+}*/
+
+for(let i =0; i<rows;i++){
+    for(let j=0;j<cols;j++){
+        const block = document.createElement('div');
+    block.classList.add("block")
+    board.appendChild(block);
+    }
 }
