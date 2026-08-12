392c452403d6ea3401f3e2eea637372d679863e3
Anisha <ani25sha@gmail.com>
2026-08-12 08:16:03 +0530

    snake body

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const board = document.querySelector('.board');
-const blockHeight = 30
-const blockWidth = 30
+const blockHeight = 50
+const blockWidth = 50
 
 const cols=Math.floor(board.clientWidth/blockWidth);
 const rows=Math.floor(board.clientHeight/blockHeight);
@@ -10,12 +10,25 @@ const rows=Math.floor(board.clientHeight/blockHeight);
     block.classList.add("block")
     board.appendChild(block);
 }*/
+const blocks =[];
+
+let a = Math.floor(Math.random()*rows);
+let b =  Math.floor(Math.random()*cols);
+ 
+const snake =[{x:1, y:2},{x:1,y:3},{x:1,y:4}];
+
 
 for(let i =0; i<rows; i++){
     for(let j=0; j<cols; j++){
         const block = document.createElement('div');
         block.classList.add("block")
         board.appendChild(block);
-        block.innerText=`${i}-${j}`;
+        blocks[`${i}-${j}`]= block;
     }
+}
+
+function render(){
+    snake.forEach(segment =>{
+        blocks[`${segment.x}-${segment.y}`].classList.add("fill");
+    })
 }
